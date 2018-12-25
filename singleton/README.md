b94574bfc1b90811cb5ae289484725cbbdcbfe43
umaumax <umauma7083@gmail.com>
2018-12-26 00:54:39 +0900

    Add singleton example

---
+++ singleton/README.md
@@ -0,0 +1,4 @@
+# c++ singleton
+
+* [シングルトンのベターな実装方法 \- Qiita]( https://qiita.com/kikuuuty/items/fcf5f7df2f0493c437dc )
+* [ゲームプログラマのためのデザインパターン（シングルトン） \- Qiita]( https://qiita.com/narumi_/items/b205e59e7fc81695f380 )
---
+++ singleton/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+class Hoge {
+ public:
+  Hoge() : val_(123) {}
+  ~Hoge() {}
+
+  void Print() { std::cout << val_ << std::endl; }
+  void SetVal(int val) { val_ = val; }
+
+ private:
+  int val_;
+};
+
+template <class T>
+class Singleton {
+ public:
+  static inline T& GetInstance() {
+    static T instance;
+    return instance;
+  }
+
+ private:
+  Singleton()  = default;
+  ~Singleton() = default;
+
+  Singleton(const Singleton&) = delete;
+  Singleton& operator=(const Singleton&) = delete;
+  Singleton(Singleton&&)                 = delete;
+  Singleton& operator=(Singleton&&) = delete;
+};
+
+int main(int argc, char* argv[]) {
+  {
+    Hoge& hoge = Singleton<Hoge>::GetInstance();
+    hoge.Print();
+    hoge.SetVal(456);
+  }
+  {
+    Hoge& hoge = Singleton<Hoge>::GetInstance();
+    hoge.Print();
+    hoge.SetVal(456);
+  }
+  return 0;
+}
