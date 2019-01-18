f9f4b49f550dd398199c288d2b15e5d9113e995c
umaumax <umauma7083@gmail.com>
2019-01-19 05:00:30 +0900

    Refactor access private field example

--- binary-hacks/access-private-field/main.cpp
+++ binary-hacks/access-private-field/main.cpp
@@ -3,7 +3,10 @@
 struct Hoge {
  private:
   void func0() { std::cout << "func0:" << std::endl; }
-  void func1(int a) { std::cout << "func1:" << a << std::endl; }
+  int func1(int a) {
+    std::cout << "func1:" << a << std::endl;
+    return 1;
+  }
   void func2(int a, int b) { std::cout << "func2:" << a << "," << b << std::endl; }
   int member_;
 };
@@ -11,19 +14,16 @@ struct Hoge {
 #include "private_accesor.hpp"
 // method
 DECL_PRIVATE_METHOD_ACCESSOR(Hoge, func0, void, ())
-DECL_PRIVATE_METHOD_ACCESSOR(Hoge, func1, void, (int))
+DECL_PRIVATE_METHOD_ACCESSOR(Hoge, func1, int, (int))
 DECL_PRIVATE_METHOD_ACCESSOR(Hoge, func2, void, (int, int))
-#define PRIVATE_CALL_Hoge_func0(x) ((x).*result<Hoge_func0>::ptr)
-#define PRIVATE_CALL_Hoge_func1(x) ((x).*result<Hoge_func1>::ptr)
-#define PRIVATE_CALL_Hoge_func2(x) ((x).*result<Hoge_func2>::ptr)
 // member
 DECL_PRIVATE_MEMBER_ACCESSOR(Hoge, member_, int)
 
 int main() {
   Hoge a;
-  PRIVATE_CALL_Hoge_func0(a)();
-  PRIVATE_CALL_Hoge_func1(a)(1);
-  PRIVATE_CALL_Hoge_func2(a)(1, 2);
+  PRIVATE_METHOD_CALL_Hoge_func0(a);
+  PRIVATE_METHOD_CALL_Hoge_func1(a, 1);
+  PRIVATE_METHOD_CALL_Hoge_func2(a, 1, 2);
   PRIVATE_ACCESS_Hoge_member_(a) = 123;
   std::cout << "member_:" << PRIVATE_ACCESS_Hoge_member_(a) << std::endl;
 }
--- binary-hacks/access-private-field/private_accesor.hpp
+++ binary-hacks/access-private-field/private_accesor.hpp
@@ -27,17 +27,21 @@ typename rob<Tag, p>::filler rob<Tag, p>::filler_obj;
 //
 
 #include <functional>
-#define DECL_PRIVATE_METHOD_ACCESSOR(class_name, target_name, target_type1, target_type2) \
-  struct class_name##_##target_name {                                                     \
-    using type = target_type1(class_name::*) target_type2;                                \
-  };                                                                                      \
-  template class rob<class_name##_##target_name, &class_name::target_name>;
+#define DECL_PRIVATE_METHOD_ACCESSOR(class_name, method_name, return_type, args_type)    \
+  struct class_name##_##method_name {                                                    \
+    using type = return_type(class_name::*) args_type;                                   \
+  };                                                                                     \
+  template class rob<class_name##_##method_name, &class_name::method_name>;              \
+  template <class F, class... Args>                                                      \
+  return_type PRIVATE_METHOD_CALL_##class_name##_##method_name(F &&x, Args &&... args) { \
+    return (return_type)((x).*result<class_name##_##method_name>::ptr)(args...);         \
+  }
 
-#define DECL_PRIVATE_MEMBER_ACCESSOR(class_name, target_name, target_type)  \
-  struct class_name##_##target_name {                                       \
-    using type = target_type class_name::*;                                 \
+#define DECL_PRIVATE_MEMBER_ACCESSOR(class_name, member_name, member_type)  \
+  struct class_name##_##member_name {                                       \
+    using type = member_type class_name::*;                                 \
   };                                                                        \
-  template class rob<class_name##_##target_name, &class_name::target_name>; \
-  target_type& PRIVATE_ACCESS_##class_name##_##target_name(class_name& obj) { return obj.*result<class_name##_##target_name>::ptr; }
+  template class rob<class_name##_##member_name, &class_name::member_name>; \
+  member_type &PRIVATE_ACCESS_##class_name##_##member_name(class_name &obj) { return obj.*result<class_name##_##member_name>::ptr; }
 
 #endif  // PRIVATE_ACCESOR_HPP_INCLUDED__
