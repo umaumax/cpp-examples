7992cbf65d9b55d12a790ef4532d1feccf7068bb
umaumax <umauma7083@gmail.com>
2019-03-21 00:57:32 +0900

    Add libxml2 example code

---
+++ libxml2/README.md
@@ -0,0 +1,18 @@
+# libxml2
+
+* [The XML C parser and toolkit of Gnome]( http://xmlsoft.org/ )
+  * > it is free software available under the MIT License.
+
+## doc
+* [Module tree from libxml2]( http://www.xmlsoft.org/html/libxml-tree.html )
+
+## NOTE
+* [xmlCleanupParser]( http://www.xmlsoft.org/html/libxml-parser.html#xmlCleanupParser )
+  * no multi-threaded support
+* [xmlInitParser]( http://www.xmlsoft.org/html/libxml-parser.html#xmlInitParser )
+  * no multi-threaded support
+
+## FYI
+* [libxml\+\+]( http://libxmlplusplus.sourceforge.net/ )
+  * WARNING
+  * > Libxml++ is released under the LGPL version 2 or above
---
+++ libxml2/main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "libxml/parser.h"
+
+namespace libxml_util {
+inline std::string GetName(const xmlNode *node) {
+  return std::string(reinterpret_cast<const char *>(node->name));
+}
+inline std::string GetProp(const xmlNode *node, const std::string &name) {
+  const xmlChar *xml_char = reinterpret_cast<const xmlChar *>(name.c_str());
+  xmlAttrPtr xml_attr_ptr = xmlHasProp(node, xml_char);
+  if (xml_attr_ptr == nullptr) {
+    return "";
+  }
+  xmlChar *xml_prop_char = xmlGetProp(node, xml_char);
+  std::string prop(reinterpret_cast<char *>(xml_prop_char));
+  xmlFree(xml_prop_char);
+  return prop;
+}
+
+template <class... Args>
+inline std::vector<xmlNodePtr> CurNextEach(xmlNodePtr cur, Args... args) {
+  std::vector<xmlNodePtr> inputs = {cur};
+  std::vector<xmlNodePtr> outputs;
+  std::vector<std::function<xmlNodePtr(xmlNodePtr cur)>> funcs = {args...};
+  for (auto &&f : funcs) {
+    for (auto &&cur : inputs) {
+      for (; cur != nullptr; cur = cur->next) {
+        xmlNodePtr cur_ret = f(cur);
+        if (cur_ret != nullptr) {
+          outputs.emplace_back(cur_ret);
+        };
+      }
+    }
+    // NOTE: for next inputs
+    inputs = outputs;
+    outputs.clear();
+  }
+  return inputs;
+}
+}  // namespace libxml_util
+
+int main(int argc, char *argv[]) {
+  // NOTE: no multi-threaded support
+  xmlInitParser();
+
+  xmlDocPtr doc;
+  xmlNodePtr cur;
+
+  if (argc <= 1) {
+    std::cerr << "Usage: " << argv[0] << "[xml filepath]" << std::endl;
+    return 0;
+  }
+
+  std::string docname(argv[1]);
+  doc = xmlParseFile(docname.c_str());
+  if (!doc) {
+    std::cerr << "[libxml] xmlParseFile error :" << docname << std::endl;
+    return 1;
+  }
+
+  std::map<std::string, std::string> argument_map;
+  cur                    = xmlDocGetRootElement(doc);
+  xmlNodePtr objects_cur = cur->xmlChildrenNode->next->xmlChildrenNode;
+  // NOTE: chain method version
+  {
+    libxml_util::CurNextEach(
+        objects_cur,
+        // NOTE: objects loop
+        [](xmlNodePtr cur) {
+          return libxml_util::GetName(cur) == "object" &&
+                         libxml_util::GetProp(cur, "key") == "test"
+                     ? cur->xmlChildrenNode->next->next->next->xmlChildrenNode
+                     : nullptr;
+        },
+        // NOTE: arguments loop
+        [&](xmlNodePtr cur) {
+          if (libxml_util::GetName(cur) == "argument") {
+            std::string argument_key   = libxml_util::GetProp(cur, "name");
+            std::string argument_value = libxml_util::GetProp(cur, "value");
+            std::cout << "key:" << argument_key << std::endl;
+            std::cout << "value:" << argument_value << std::endl;
+            argument_map[argument_key] = argument_value;
+          }
+          return nullptr;
+        });
+  }
+  // NOTE: normal version
+  {
+    for (; objects_cur != nullptr; objects_cur = objects_cur->next) {
+      if (libxml_util::GetName(objects_cur) != "object" ||
+          libxml_util::GetProp(objects_cur, "key") != "test") {
+        continue;
+      }
+      xmlNodePtr arguments_cur =
+          objects_cur->xmlChildrenNode->next->next->next->xmlChildrenNode;
+      for (; arguments_cur != nullptr; arguments_cur = arguments_cur->next) {
+        if (libxml_util::GetName(arguments_cur) != "argument") {
+          continue;
+        }
+        std::string argument_key = libxml_util::GetProp(arguments_cur, "name");
+        std::string argument_value =
+            libxml_util::GetProp(arguments_cur, "value");
+        std::cout << "key:" << argument_key << std::endl;
+        std::cout << "value:" << argument_value << std::endl;
+        argument_map[argument_key] = argument_value;
+      }
+    }
+  }
+  xmlFreeDoc(doc);
+
+  // NOTE: no multi-threaded support
+  xmlCleanupParser();
+  return 0;
+}
