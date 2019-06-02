a35ce8468272681c6317258036162cd9b3dd472f
umaumax <umauma7083@gmail.com>
2019-06-02 21:46:58 +0900

    Add boost asio thread pool example

---
+++ boost/thread_pool/README.md
@@ -0,0 +1,12 @@
+# boost thread_pool
+
+## how to build
+```
+g++ -std=c++11 main.cpp -lboost_thread-mt
+```
+
+## FYI
+* [スレッドプールの実装方法について]( http://mojavy.com/blog/2014/03/03/implementing-thread-pool/ )
+* [\[C\+\+11\]\[Boost\] boost::asio::io\_serviceを別スレッドでrunする – No Coding, No Life\.]( https://ncnl.blog.so-net.ne.jp/2016-02-13 )
+* [c\+\+ \- Boost thread error: undefined reference \- Stack Overflow]( https://stackoverflow.com/questions/3584365/boost-thread-error-undefined-reference )
+* [c\+\+ \- Boost::ASIO and std::packaged\_task \- Stack Overflow]( https://stackoverflow.com/questions/35611846/boostasio-and-stdpackaged-task )
---
+++ boost/thread_pool/main.cpp
@@ -0,0 +1,54 @@
+#include <functional>
+#include <iostream>
+#include <thread>
+
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/future.hpp>
+
+int add(int a, int c) { return a + c; }
+int hello(std::string name) {
+  std::cout << "Hello " << name << std::endl;
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+  boost::asio::io_service io_service;
+  boost::asio::io_service::work work(io_service);
+  std::vector<std::thread> thread_pool;
+
+  int thread_pool_n = 3;
+  for (size_t i = 0; i < thread_pool_n; i++) {
+    thread_pool.emplace_back([&io_service] { io_service.run(); });
+  }
+
+  // NOTE: with return value
+  boost::packaged_task<int> pt(boost::bind(add, 1, 2));
+  // NOTE: io_service post requirements
+  // The handler to be called. The io_service will make a copy of the handler object as required.
+  // The function signature of the handler must be: void handler();
+  io_service.post(std::bind([&] { pt(); }));
+  boost::unique_future<int> uf = pt.get_future();
+  std::cout << uf.get() << std::endl;
+
+  int task_n = 5;
+  std::vector<boost::packaged_task<int>> pts(task_n);
+  for (size_t i = 0; i < task_n; i++) {
+    // NOTE: without return value
+    io_service.post(std::bind(hello, "boost"));
+    // NOTE: with return value
+    pts[i] = boost::packaged_task<int>(boost::bind(add, i, i));
+    io_service.post(std::bind([&, i] { pts[i](); }));
+  }
+
+  std::cout << "This is Main Thread!" << std::endl;
+
+  for (size_t i = 0; i < task_n; i++) {
+    std::cout << "add() result = " << pts[i].get_future().get() << std::endl;
+  }
+
+  io_service.stop();
+  for (std::thread& t : thread_pool) {
+    t.join();
+  }
+}
