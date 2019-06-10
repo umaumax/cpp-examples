c328b8af922b6a990a3a544aeebf674c9274e086
umaumax <umauma7083@gmail.com>
2019-06-11 00:09:02 +0900

    Update example of boost thread_pool

--- boost/thread_pool/README.md
+++ boost/thread_pool/README.md
@@ -8,8 +8,14 @@ g++ -std=c++11 main.cpp -lboost_thread-mt
 g++ -std=c++11 main.cpp -lpthread -lboost_thread -lboost_system
 ```
 
+## TODO
+* `boost::packaged_task<int> pt(boost::bind(func, xxx));`
+  * WARN: 指定した関数の引数はrefではなく値コピー?になるようなので．注意
+
 ## FYI
 * [スレッドプールの実装方法について]( http://mojavy.com/blog/2014/03/03/implementing-thread-pool/ )
 * [\[C\+\+11\]\[Boost\] boost::asio::io\_serviceを別スレッドでrunする – No Coding, No Life\.]( https://ncnl.blog.so-net.ne.jp/2016-02-13 )
 * [c\+\+ \- Boost thread error: undefined reference \- Stack Overflow]( https://stackoverflow.com/questions/3584365/boost-thread-error-undefined-reference )
 * [c\+\+ \- Boost::ASIO and std::packaged\_task \- Stack Overflow]( https://stackoverflow.com/questions/35611846/boostasio-and-stdpackaged-task )
+* [c\+\+ \- THREAD ERROR: invalid use of non\-static member function \- Stack Overflow]( https://stackoverflow.com/questions/41476077/thread-error-invalid-use-of-non-static-member-function )
+  * bindする関数にclassのmethodを指定する方法
--- boost/thread_pool/main.cpp
+++ boost/thread_pool/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <functional>
 #include <iostream>
 #include <thread>
@@ -11,6 +12,10 @@ int hello(std::string name) {
   std::cout << "Hello " << name << std::endl;
   return 0;
 }
+void str_upper(std::string& str) {
+  std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+  std::cout << "str_upper called: result=" << str << std::endl;
+}
 
 int main(int argc, char* argv[]) {
   boost::asio::io_service io_service;
@@ -22,15 +27,29 @@ int main(int argc, char* argv[]) {
     thread_pool.emplace_back([&io_service] { io_service.run(); });
   }
 
-  // NOTE: with return value
-  boost::packaged_task<int> pt(boost::bind(add, 1, 2));
-  // NOTE: io_service post requirements
-  // The handler to be called. The io_service will make a copy of the handler object as required.
-  // The function signature of the handler must be: void handler();
-  io_service.post(std::bind([&] { pt(); }));
-  boost::unique_future<int> uf = pt.get_future();
-  std::cout << uf.get() << std::endl;
+  // NOTE: one task example
+  {
+    // NOTE: with return value
+    boost::packaged_task<int> pt(boost::bind(add, 1, 2));
+    // NOTE: io_service post requirements
+    // The handler to be called. The io_service will make a copy of the handler object as required.
+    // The function signature of the handler must be: void handler();
+    io_service.post(std::bind([&] { pt(); }));
+    boost::unique_future<int> uf = pt.get_future();
+    std::cout << "add(1,2)=" << uf.get() << std::endl;
+  }
 
+  // NOTE: one task example with copying ref args
+  {
+    std::string msg = "nanoha";
+    boost::packaged_task<void> pt(boost::bind(str_upper, msg));
+    io_service.post(std::bind([&] { pt(); }));
+    boost::unique_future<void> uf = pt.get_future();
+    uf.get();
+    std::cout << "msg:" << msg << std::endl;
+  }
+
+  // NOTE: multi tasks example
   int task_n = 5;
   std::vector<boost::packaged_task<int>> pts(task_n);
   for (size_t i = 0; i < task_n; i++) {
