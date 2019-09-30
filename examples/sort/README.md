d905175f873d885506626fd97d40962ceb967093
umaumax <umauma7083@gmail.com>
2019-09-30 21:03:19 +0900

    Add nth_element examples

--- examples/sort/README.md
+++ examples/sort/README.md
@@ -2,3 +2,8 @@
 
 ## top_n_sort
 [c\+\+ \- Use std::sort to find top N items in a std::vector \- Stack Overflow]( https://stackoverflow.com/questions/4391372/use-stdsort-to-find-top-n-items-in-a-stdvector )
+
+[partial\_sort \- cpprefjp C\+\+日本語リファレンス]( https://cpprefjp.github.io/reference/algorithm/partial_sort.html )
+> なお、トップ10がどれかわかれば十分である（1位から10位までは順不同でよい）ような場合、nth_element()が使用できる。
+
+[nth\_element \- cpprefjp C\+\+日本語リファレンス]( https://cpprefjp.github.io/reference/algorithm/nth_element.html )
--- examples/sort/top_n_sort.cpp
+++ examples/sort/top_n_sort.cpp
@@ -1,39 +1,61 @@
 #include <algorithm>
-#include <array>
 #include <functional>
 #include <iostream>
 #include <random>
 #include <string>
+#include <vector>
 
 int main(int argc, char* argv[]) {
   constexpr int N = 10;
+  int loop_n      = 10;
   int top_n       = 3;
 
-  std::array<int, N> input;
+  std::vector<int> input(N);
   for (int i = 0; i < N; i++) {
     input[i] = i;
   }
 
-  std::random_device seed_gen;
-  std::mt19937 engine(seed_gen());
-  std::shuffle(input.begin(), input.end(), engine);
+  // std::cout << "input data:" << std::endl;
+  // for (auto&& e : input) {
+  // std::cout << e << ", ";
+  // }
+  // std::cout << std::endl;
 
-  std::cout << "input data:" << std::endl;
-  for (auto&& e : input) {
-    std::cout << e << ", ";
-  }
-  std::cout << std::endl;
-
-  std::partial_sort(input.begin(), input.begin() + top_n, input.end());
   std::cout << "std::partial_sort by " << top_n << std::endl;
-  for (auto&& e : input) {
-    std::cout << e << ", ";
-  }
-  std::cout << std::endl;
+  for (int i = 0; i < loop_n; i++) {
+    std::random_device seed_gen;
+    std::mt19937 engine(seed_gen());
+    std::shuffle(input.begin(), input.end(), engine);
 
-  for (int i = 0; i < top_n; i++) {
-    if (input[i] != i) {
-      std::cout << "input[" << i << "] != " << i << std::endl;
+    std::partial_sort(input.begin(), input.begin() + top_n, input.end());
+    for (auto&& e : input) {
+      std::cout << e << ", ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < top_n; i++) {
+      if (input[i] != i) {
+        std::cout << "input[" << i << "] != " << i << std::endl;
+      }
+    }
+  }
+
+  std::cout << "std::nth_element by " << top_n << std::endl;
+  for (int i = 0; i < loop_n; i++) {
+    std::random_device seed_gen;
+    std::mt19937 engine(seed_gen());
+    std::shuffle(input.begin(), input.end(), engine);
+
+    std::nth_element(input.begin(), input.begin() + top_n, input.end());
+    for (auto&& e : input) {
+      std::cout << e << ", ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < top_n; i++) {
+      if (input[i] > top_n) {
+        std::cout << "input[" << i << "] >= " << top_n << std::endl;
+      }
     }
   }
   return 0;
